4595f324160fafaae209415e6437443ec341718d
John Tan <johntan@r-78-177-28-172.comp.nus.edu.sg>
2012-09-24 11:49:50 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// console.log("main.js is loaded");
+$(document).ready(function(){
+	//here.
+	$("img").on("dblclick", function(){
+		//here.
+		$("img").width(200);
+
+		$("h1").fadeout();
+
+		$(".booboo").slideup();
+
+		$("#rahul").hide();
+	});
+	});
+
+	//$("img").click(function){
+		//here.
+		//$("img").width(200);
+	//});
+});
